ea190a9a5635938fecb519362c7e32ef203b2316
Andrey Kopitsa <andrey.kopitsa@gmail.com>
2017-12-19 11:37:18 +0200

    Add changes

--- manifests/conf.pp
+++ manifests/conf.pp
@@ -1,9 +1,9 @@
 class wordpress::conf {
   $root_password = 'password'
-  $db_name = 'wordpress'
-  $db_user = 'wp'
-  $db_user_password = 'password'
-  $db_host = 'localhost'
+#  $db_name = 'wordpress'
+#  $db_user = 'wp'
+#  $db_user_password = 'password'
+#  $db_host = 'localhost'
   $file_name = "latest.tar.gz"
   $db_user_host = "${db_user}@${db_host}"
   $db_user_host_db = "${db_user}@${db_host}/${db_name}.*"
